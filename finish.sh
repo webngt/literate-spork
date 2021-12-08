c3e724d49d1e6f48b3ac70ba180e061af11ed388
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2021-12-08 14:48:49 +0300

    debug commit

--- finish.sh
+++ finish.sh
@@ -1,31 +1,39 @@
-#!/bin/sh
+#!/bin/bash
 
 mod_path="/root"
 
 modules=$(find $mod_path -type d -name go -prune -o -name go.mod -print)
 
 # unexpected error while finding modules
-[ "$?" = "1" ] && jq --null-input --arg error "$modules" '{"error": $msg, "result": "" }' && exit 1
+[ "$?" = "1" ] && jq --null-input --arg error "$modules" '{"error": $error, "result": "" }' && exit 1
 
 # no module found
-[ "$modules" = "" ] && jq --null-input --arg error "no module found in path $mod_path" '{"error": $msg, , "result": "" }' && exit 1
+[ "$modules" = "" ] && jq --null-input --arg error "no module found in path $mod_path" '{"error": $error, "result": "" }' && exit 1
 
 
 errors=""
 result=""
 
+
 for module in $modules
 do
-    d=$(dirname $module)
+    echo $(dirname $module)
+    
+    cd $(dirname $module)
 
     # run linter
-    l = $(golangci-lint run $d)
+    l=$(golangci-lint run ./...)
+
+    echo "lintextit=$?"
 
     [ "$?" = "1" ] && errors="$errors$l"
     [ "$?" = "0" ] && result="$result$l"
 
     # run unit tests
-    t = $(go test $d)
+    t=$(go test ./...)
+
+    echo "testexit=$?"
+
 
     [ "$?" = "1" ] && errors="$errors$t"
     [ "$?" = "0" ] && result="$result$t"
@@ -33,4 +41,7 @@ do
 
 done
 
-jq --null-input --arg error "$errors" --arg result="$result" '{"error": $errors, , "result": $result }'
+#echo -n "$error"
+echo -n "$result"
+
+#jq --null-input --arg error "$errors" --arg result="$result" '{"error": $errors, "result": $result }'
