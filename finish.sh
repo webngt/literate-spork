1be68292eeed9d7172e88b87037b42f704bd9736
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2021-12-08 17:09:53 +0300

    structure linter and unit tests outputs

--- finish.sh
+++ finish.sh
@@ -6,35 +6,26 @@ modules=$(find $mod_path -type d -name go -prune -o -name go.mod -print)
 res=$?
 
 # unexpected error while finding modules
-[ $res -eq 1 ] && jq --null-input --arg error "$modules" '{"error": $error, "result": "" }' && exit 1
+[ $res -eq 1 ] && jq --null-input --arg error "$modules" '{"error": $error }' && exit 1
 
 # no module found
-[ "$modules" = "" ] && jq --null-input --arg error "no module found in path $mod_path" '{"error": $error, "result": "" }' && exit 1
-
-
-errors=""
-result=""
+[ "$modules" = "" ] && jq --null-input --arg error "no module found in path $mod_path" '{"error": $error }' && exit 1
 
 
 for module in $modules
-do
-    echo $(dirname $module)
-    
+do    
     cd $(dirname $module)
 
     # run linter
-    l=$(golangci-lint run ./...)
-    res=$?
-
-    [ $res -eq 1 ] && errors="$errors$l" || result="$result$l"
+    linter_out=$(golangci-lint run ./...)
 
     # run unit tests
-    t=$(go test ./...)
+    unit_test_fail=""
+    unit_test_ok=""
+    unit_test_out=$(go test .)
     res=$?
 
-    [ $res -eq 1 ] && errors="$errors$t" || result="$result$t"
+    [ $res -eq 1 ] && unit_test_fail=$unit_test_out || unit_test_ok=$unit_test_out
 
-
-done
-
-jq --null-input --arg error "$errors" --arg result "$result" '{"error": $error, "result": $result }'
+    jq --null-input --arg linter_out "$linter_out" --arg unit_test_fail "$unit_test_fail" --arg unit_test_ok "$unit_test_ok" '{"linter_out": $linter_out, "unit_test_fail": $unit_test_fail, "unit_test_ok", $unit_test_ok }'
+done | jq -n '.results |= [inputs]'
