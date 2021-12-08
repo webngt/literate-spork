01487e178f4e6e305285bba6175d0054c87bf9c7
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2021-12-08 16:27:49 +0300

    fix retval usage

--- finish.sh
+++ finish.sh
@@ -3,9 +3,10 @@
 mod_path="/root"
 
 modules=$(find $mod_path -type d -name go -prune -o -name go.mod -print)
+res=$?
 
 # unexpected error while finding modules
-[ "$?" = "1" ] && jq --null-input --arg error "$modules" '{"error": $error, "result": "" }' && exit 1
+[ $res -eq 1 ] && jq --null-input --arg error "$modules" '{"error": $error, "result": "" }' && exit 1
 
 # no module found
 [ "$modules" = "" ] && jq --null-input --arg error "no module found in path $mod_path" '{"error": $error, "result": "" }' && exit 1
@@ -23,25 +24,17 @@ do
 
     # run linter
     l=$(golangci-lint run ./...)
+    res=$?
 
-    echo "lintextit=$?"
-
-    [ "$?" = "1" ] && errors="$errors$l"
-    [ "$?" = "0" ] && result="$result$l"
+    [ $res -eq 1 ] && errors="$errors$l" || result="$result$l"
 
     # run unit tests
     t=$(go test ./...)
+    res=$?
 
-    echo "testexit=$?"
-
-
-    [ "$?" = "1" ] && errors="$errors$t"
-    [ "$?" = "0" ] && result="$result$t"
+    [ $res -eq 1 ] && errors="$errors$t" || result="$result$t"
 
 
 done
 
-#echo -n "$error"
-echo -n "$result"
-
-#jq --null-input --arg error "$errors" --arg result="$result" '{"error": $errors, "result": $result }'
+jq --null-input --arg error "$errors" --arg result "$result" '{"error": $error, "result": $result }'
